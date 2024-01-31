fd43a1f509728a405808fceb412b12450ef45d6a
librasvii <moreiralucas511@gmail.com>
2024-01-30 21:29:24 -0300

    first commit

---
+++ insertion_sort/1.sh
@@ -0,0 +1,9 @@
+#!/bash/bin
+
+arr=(2 4 6 1 3 5)
+#key=0
+
+for x in "${arr[@]}";
+do
+    echo "$x"
+done
---
+++ random_projects/for_command/1.sh
@@ -0,0 +1,53 @@
+#!/bash/bin
+
+# Simple count number
+for i in {0..9}
+do
+    echo "Number: $i"
+done
+
+############################
+# Count by five
+printf "\n"
+for i in {0..20..5};
+do
+    echo "Number: $i"
+done
+
+#############################
+# One count function
+function count() {
+    for x in {0..10}
+    do
+        echo "Number: $x"
+    done
+}
+
+# Call the function
+count
+
+############################
+printf "\n"
+
+myvar="9"
+
+function multi_var() {
+    # declared local variable
+    # who multi for yourself by five
+    local myvar="10"
+    ((myvar=myvar*5))
+
+    echo $myvar
+}
+
+multi_var
+
+echo $myvar
+
+printf "\n"
+
+# Count the alfa
+for y in {a..z}
+do
+    echo "Alfa: $y"
+done

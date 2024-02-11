016d53b04deed4420544aabba134d7127d000b95
librasvii <moreiralucas511@gmail.com>
2024-02-11 10:07:31 -0300

    update

--- insertion_sort/v1.sh
+++ insertion_sort/v1.sh
@@ -1,14 +1,41 @@
 #!/bash/bin
 
-arr=(2 4 6 1 3 5)
+n=5
+arr=(3 0 4 2 1)
 
-lenn=${arr[*]}
-j=0
-
-for x in "${arr[@]}"; do
-    key=$((x - 1))
-    while [ $j -lt 0 ] && [ $j -lt "${lenn[j]}" ]; do
-         arr=$((j + 1))=key
-        #echo "${arr=((j + 1))=key}"
+# Perform insertion sort to sort the array
+j=1
+while [ $j -lt $n ]
+do
+    c=0
+    k=$((j - 1))
+    while [ "$k"  -ge 0 ]
+    do
+        if [ "${arr[k]}" -ge "${arr[j]}" ]
+        then
+            c=$((c + 1))
+        fi
+    k=$((k - 1))
     done
+
+    x=$j
+    y=$((j - 1))
+
+    while [ "$c" -gt 0 ]
+    do
+        # Swapping the elements
+        temp=${arr[x]}
+        arr[x]=${arr[y]}
+        arr[y]=$temp
+
+        x=$((x - 1))
+        y=$((y - 1))
+        c=$((c - 1))
+
+    done
+
+    j=$((j + 1))
 done
+
+# Print the sorted array
+echo "${arr[*]}"
